e1fecb5f871cb4054723f54e2ea57442cc4fbcac
Samuel Gonzalez <xame2005@gmail.com>
2021-02-12 00:15:32 -0600

    [CREATED] website layout (Header and Footer)

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../styles/components/Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <p className="Footer-title">E-Commerce Merch</p>
+      <p className="Footer-copy">Derechos Reservados</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import '../styles/components/Layout.css';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="Main">
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
