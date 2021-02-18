7a3e6ef3f884edd9d213b03db35d5436954db3f4
Samuel Gonzalez <xame2005@gmail.com>
2021-02-17 23:20:34 -0600

    [CREATED] Integration of useContext hook

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -3,12 +3,14 @@ import initialState from '../initialState';
 
 const useInitialState = () => {
   const [state, setState] = useState(initialState);
+
   const addToCart = (payload) => {
     setState({
       ...state,
       cart: [...state.cart, payload],
     });
   };
+
   const removeFromCart = (payload) => {
     setState({
       ...state,
