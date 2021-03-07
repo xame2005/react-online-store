65e4df14984c32293ae7999f00356cfef0be73c4
Samuel Gonzalez <xame2005@gmail.com>
2021-03-07 17:42:39 -0600

    [FIXED] Google Maps useGoogleAddress fixed and Firebase Integration

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-const Map = () => {
-  const MapStyle = {
+const Map = ({ data }) => {
+  const MapStyles = {
     height: '50vh',
     width: '100%',
   };
 
   const defaulCenter = {
-    lat: 19.4267261,
-    lng: -99.1718796,
+    lat: data.lat,
+    lng: data.lng,
   };
   return (
-    <LoadScript googleMapsApiKey="AIzaSyAFE2knAk0G94v7lam8dYvsnJfLw6iK59s">
-      <GoogleMap mapContainerStyle={MapStyle} zoom={9} center={defaulCenter}>
+    <LoadScript googleMapsApiKey="AIzaSyC7iixOm1LmHz9FD381ep7PEPoeTdXPCnA">
+      <GoogleMap mapContainerStyle={MapStyles} zoom={9} center={defaulCenter}>
         <Marker position={defaulCenter} />
       </GoogleMap>
     </LoadScript>
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -2,17 +2,19 @@ import React, { useContext } from 'react';
 import '../styles/components/Success.css';
 import AppContext from '../context/AppContext';
 import Map from '../components/Map';
+import useGoogleAddress from '../hooks/useGoogleAddress';
 
 const Home = () => {
   const { state } = useContext(AppContext);
   const { buyer } = state;
+  const location = useGoogleAddress(buyer[0].address);
   return (
     <div className="Success">
       <div className="Success-content">
-        <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
+        <h2>{`${buyer[0].name}, Gracias por tu compra`}</h2>
         <span>Tu pedido llegará en 3 días a tu dirección</span>
-        <div className="Success map">
-          <Map />
+        <div className="Success-map">
+          <Map data={location} />
         </div>
       </div>
     </div>
